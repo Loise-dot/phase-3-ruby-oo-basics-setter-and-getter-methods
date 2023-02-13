a3a8b6f55eadc6a69a3d2410991e279762f2841b
Loise <loise.wambui@student.moringaschool.com>
2023-02-13 19:28:36 +0300

    Done

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,14 @@
+require "pry"
 class Dog
-
+    attr_accessor :breed, :name
+    def name=(name)
+        @name = name
+        @breed = breed
+    end
 end
+harry = Dog.new
+
+harry.name = "Harry"
+harry.breed = "German"
+puts harry.name
+puts harry.breed
--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,17 @@
+require "pry"
 class Person
-
+    attr_accessor :name, :job
+    def name=(name)
+        @name = name
+        @job = job
+    end
 end
+
+
+pug = Person.new
+pug.name = ("Pug")
+pug.job = ("Driver")
+puts pug.name
+puts pug.job
+
+# binding.pry
